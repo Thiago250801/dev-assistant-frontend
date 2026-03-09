bc692b45137e14471ab4132bda2d136907651005
Thiago Arthur <thlimabsgim@gmail.com>
2026-03-09 11:31:10 -0400

    feat(app): compose main layout with sidebar and chat panes

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Sidebar from './components/Sidebar'
+import Chat from './components/Chat'
+
+function App() {
+
+  return (
+    <div className="h-screen flex">
+      <Sidebar />
+      <Chat />
+    </div>
+  )
+}
+
+export default App
